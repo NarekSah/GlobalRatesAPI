e6f8e51a1cbb4f7b969b8f5719f95f11cb5d67c5
Narek Sahakyan <Narek.Sahakyan@acba.am>
2021-12-03 17:51:39 +0400

    Add project files.

---
+++ RatesAPI/Controllers/RatesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RatesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatesController : ControllerBase
+    {
+        private readonly ILogger<RatesController> _logger;
+        public RatesController(ILogger<RatesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
